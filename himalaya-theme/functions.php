15fa7ca025b1e114deeda28a75d584d779fc4870
Anshoriee <kamalanshary123@gmail.com>
2026-05-19 14:56:39 +0700

    edit tema

---
+++ himalaya-theme/functions.php
@@ -0,0 +1,7 @@
+<?php
+function himalaya_theme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'himalaya_theme_setup');
---
+++ himalaya-theme/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>MPA Himalaya</title>
+
+    <link rel="stylesheet"
+        href="<?php echo get_template_directory_uri(); ?>/assets/index.css">
+</head>
+
+<body>
+
+<div id="root"></div>
+
+<script type="module"
+    src="<?php echo get_template_directory_uri(); ?>/assets/main.js"></script>
+
+</body>
+
+</html>
